9d6efeb2a388be887aeea617035f011357eac5bd
Astin2605 <avanzant@stevens.edu>
2026-02-26 19:48:52 +0000

    2/26 Updates

--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,13 @@ const int switchPin = 3;
 const int BlueButtonPin = 4;
 const int GreenButtonPin = 5;
 const int YellowButtonPin = 6;
-const int RedButtonPin = 7;
+const int RedButtonPin = 7; //verify if this is an interrupt pin
 const int LiftMotorPin = 8;
 const int BreakMotorPin = 9;
 
 
 int cycle_once();
-int break_run();
+int brake_stop();
 int jog_lift();
 int cycle_cont();
 int interrupt();
@@ -38,11 +38,11 @@ void setup(){
 
 
   //Activating pin used for lift sensor
-  pinMode(liftSensorPin, INPUT);
+  pinMode(liftSensorPin, OUTPUT);
 
 
   //Activating pin used for break run sensor
-  pinMode(breakSensorPin, INPUT);
+  pinMode(breakSensorPin, OUTPUT);
 
 
   //Activating pinn used for Key-Lock switch
@@ -50,44 +50,99 @@ void setup(){
 
 
   //Activating pin used for blue button on control panel
-  pinMode(BlueButtonPin, INPUT);
+  pinMode(BlueButtonPin, INPUT_PULLUP);
 
 
   //Activating pin used for green button on control panel
-  pinMode(GreenButtonPin, INPUT);
+  pinMode(GreenButtonPin, INPUT_PULLUP);
 
 
   //Activating pin used for yellow button on control panel
-  pinMode(YellowButtonPin, INPUT);
+  pinMode(YellowButtonPin, INPUT_PULLUP);
 
 
   //Activating pin used for red button on control panel
-  pinMode(RedButtonPin, INPUT);
+  pinMode(RedButtonPin, INPUT_PULLUP);
 
 
   //Interrupt for emergency stop button
-  attachInterrupt(digitalPinToInterrupt(7), E-stop, FALLING);
+  attachInterrupt(digitalPinToInterrupt(7), E_stop, FALLING);
+
+  //Represents sending the signal for ride to run 1 time
+}
+
+  int cycle_once(){
+  if (digitalRead(GreenButtonPin) == HIGH){
+    digitalWrite(LiftMotorPin, HIGH);
+    calculated timer when to turn off lift motor
+    break_stop();
+    key = IDLE_STATE; //some kind of light that is only lit for idle state?
+  }
+    digitalWrite(LiftMotorPin, LOW);
+  if(digitalRead(breakSensorPin) == HIGH){
+    digitalWrite(BreakMotorPin, HIGH);
+      return 0;
+    }
+  return 1;
+}
+
+//Represent sending the signal for breaks to activate
+int break_stop(){
+  if (digitalRead(breakSensorPin) == HIGH){
+      digitalWrite(BreakMotorPin, HIGH);
+    }
+  if (digitalRead(breakSensorPin) == LOW){
+      digitalWrite(BreakMotorPin, LOW);
+    }
+  }
+
+//Represents sending the signal for lift to be manually activated
+//maybe change to while statement
+int jog_lift(){
+  if (digitalRead(YellowButtonPin) == HIGH){
+    digitalWrite(LiftMotorPin, HIGH);
+  else if (digitalRead(YellowButtonPin) == LOW){
+      digitalWrite(LiftMotorPin, LOW);    
+    }
+  }
+  return;
+}
 
 
-  // E-stop function definition
-  void E-stop() {
+//Represents sending the signal for ride to run continously
+int cycle_cont(){
+  if (digitalRead(liftSensorPin) == HIGH){
+      digitalWrite(LiftMotorPin, HIGH);
+      timer for experimentally found time
+      return 0;
+    }
+  return 1;
+}
+
+
+
+// E_stop function definition
+  void E_stop() {
     while (RedButtonPin == HIGH){
       if (digitalRead(breakSensorPin) == HIGH){
-      break_run; //double check what e-stop is supposed to do
-      stop lift; //keep from falling back
+        digitalWrite(LiftMotorPin, LOW);
+        brake_stop(); 
+        //Set state to idle but then issue of having key in wrong state when e_stop is released
+        //Include in protocol to remove key frrom switch and then put it back in to reset the state of the system
+        key = IDLE_STATE;
       }
     }
       if (RedButtonPin == LOW){
       return;
       }
   }
-}
-
 
 void loop() {
   
   int switchState = digitalRead(switchPin);
   
+  
+
   //manual mode state
   if (switchState == HIGH){
     key = MANUAL_STATE;
@@ -105,7 +160,7 @@ void loop() {
     }
 
     if (digitalRead(breakSensorPin) == HIGH){
-      break_run();
+      brake_stop();
     }
 //maybe change to if statement see while testing
   while(misc == true){
@@ -116,64 +171,35 @@ void loop() {
   //Auto mode functions
   if (key == AUTO_STATE && dispatch == true) {
   while (!misc) {
-    cycle_cont;
+    cycle_cont();
     if (digitalRead(breakSensorPin) == HIGH){
-      break_run;
+      break_stop();
     }
   }
   //Change dispatch to false when cycle_cont is done
   }
-}
-
-
-}
-
-
-//Represents sending the signal for ride to run 1 time
-int cycle_once(){
-  if (digitalRead(GreenButtonPin) == HIGH){
-    digitalWrite(LiftMotorPin, HIGH);
-    calculated timer when to turn off lift motor
-    digitalWrite(LiftMotorPin, LOW);
-  if(digitalRead(breakSensorPin) == HIGH){
-    digitalWrite(BreakMotorPin, HIGH);
-      return 0;
+  if (key == MANUAL_STATE && digitalRead(BlueButtonPin) == HIGH){
+      break_stop();
     }
-  return 1;
-}
-}
-//Represent sending the signal for breaks to activate
-int break_run(){
-  if (digitalRead(breakSensorPin) == HIGH){
-      digitalWrite(BreakMotorPin, HIGH);
-      return 0;
+    else if (key == MANUAL_STATE && digitalRead(GreenButtonPin) == HIGH){
+      cycle_once();
     }
-  return 1;
-}
-
-
-//Represents sending the signal for lift to be manually activated
-//maybe change to while statement
-int jog_lift(){
-  if (digitalRead(YellowButtonPin) == HIGH){
-    digitalWrite(LiftMotorPin, HIGH);
-  if (digitalRead(YellowButtonPin) == LOW){
-      return 0;
+    else if (key == MANUAL_STATE && digitalRead(YellowButtonPin) == HIGH){
+      jog_lift();
+    }
+    else if (key == AUTO_STATE && digitalRead(BlueButtonPin) == HIGH){
+      break_stop();
+    }
+    else if (key == AUTO_STATE && digitalRead(GreenButtonPin) == HIGH){
+      cycle_cont();
+    }
+    else {
+      return;
     }
   }
-  return 1;
-}
 
 
-//Represents sending the signal for ride to run continously
-int cycle_cont(){
-  if (digitalRead(liftsensorPin) == HIGH){
-      digitalWrite(LiftMotorPin, HIGH);
-      timer for experimentally found time
-      return 0;
-    }
-  return 1;
-}
+
 
 
 
